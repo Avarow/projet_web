721567d218278ad483dd0cc008c294ef9271d7ef
BANH emilie <ebanh@e105pc03.ecole.ensicaen.fr>
2018-12-13 10:24:51 +0100

    nada

--- tools/tools.js
+++ tools/tools.js
@@ -1,14 +1,22 @@
 function Grille(nbX, nbY, canvaID){
-	this.nbX=nbX;
-	this.nbY=nbY;
-	this.canva=document.getElementById(canvaID);
-	this.ctx=canva.getContext("2d");
-	this.obj=[];
-	this.nbC=nbX*nbY;
-	this.nbCur=0;
+  this.nbX=nbX;
+  this.nbY=nbY;
+  this.canva=document.getElementById(canvaID);
+  this.ctx=canva.getContext("2d");
+  this.dX = dX;
+  this.dY = dY;
+  this.tab = [][];
+  this.obj=[];
+  this.nbC=nbX*nbY;
+  this.nbCur=0;
 }
 
 
+function initialize(tab){
+  for (var i=0; i<nbX; i++)
+    for (var j=0; j<nbY; j++)
+      tab[i][j] = false;
+}
 
 
 function truncate(val)
